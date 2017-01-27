aa6cd30212fa705e648a1ab8b5808202e83abfd3
Adam Paterson <adam.paterson@meanbee.com>
2017-01-27 12:17:49 +0000

    Initial commit

---
+++ src/TestCase.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace IBM\Watson\Tests;
+
+use Mockery as m;
+use Guzzle\Http\Message\RequestInterface as GuzzleRequestInterface;
+use Guzzle\Http\Message\Response;
+use GuzzleHttp\Client as HttpClient;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
+use Guzzle\Plugin\Mock\MockPlugin;
+
+/**
+ * Class TestCase
+ * @package IBM\Watson\Common
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array
+     */
+    private $mockHttpRequests = [];
+    /**
+     * @var
+     */
+    private $mockRequest;
+    /**
+     * @var
+     */
+    private $httpClient;
+    /**
+     * @var
+     */
+    private $httpRequest;
+
+    /**
+     * Mark a request as being mocked
+     *
+     * @param GuzzleRequestInterface $request
+     * @return $this
+     */
+    public function addMockedHttpRequest(GuzzleRequestInterface $request)
+    {
+        $this->mockHttpRequests[] = $request;
+
+        return $this;
+    }
+
+    /**
+     * Get all of the mocked requests
+     *
+     * @return array
+     */
+    public function getMockedRequests()
+    {
+        return $this->mockHttpRequests;
+    }
+
+    public function getMockRequest()
+    {
+        if (null === $this->mockRequest) {
+            $this->mockRequest = m::mock('\IBM\Watson\Common\Message\RequestInterface');
+        }
+
+       return $this->mockRequest;
+    }
+
+    /**
+     * Get a mock response for a client by mock file name
+     *
+     * @param string $path Relative path to the mock response file
+     * @return Response
+     */
+    public function getMockHttpResponse($path)
+    {
+        if ($path instanceof Response) {
+            return $path;
+        }
+
+        $ref = new \ReflectionObject($this);
+        $dir = dirname($ref->getFileName());
+
+        if (!file_exists($dir.'/Mock/'.$path) && file_exists($dir.'/../Mock/'.$path)) {
+            return MockPlugin::getMockFile($dir.'/../Mock/'.$path);
+        }
+
+        return MockPlugin::getMockFile($dir.'/Mock/'.$path);
+    }
+
+    public function getHttpClient()
+    {
+        if (null === $this->httpClient) {
+            $this->httpClient = new HttpClient;
+        }
+
+        return $this->httpClient;
+    }
+
+    public function getHttpRequest()
+    {
+        if (null === $this->httpRequest) {
+            $this->httpRequest = new HttpRequest;
+        }
+
+        return $this->httpRequest;
+    }
+}
